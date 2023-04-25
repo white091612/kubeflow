419db56659f3e3fa16784071bf5014a7e093f4c4
joseph <white091612@gmail.com>
2023-04-26 02:29:14 +0900

    streamlist .py modified

--- streamlit_model_deploy.py
+++ streamlit_model_deploy.py
@@ -65,7 +65,7 @@ if uploaded_file is not None:
             st.sidebar.write("Please upload an Image to Classify")
         else:
             with st.spinner('Classifying ...'):
-                acc, result = predict_class(uploaded_file)
+                acc, result = predict_class(uploaded_file, model)
                 time.sleep(2)
                 st.success('Done!')
             st.sidebar.header("Algorithm Predicts: ")
